8cc2bcaa4f833b66ac6c13e9a769a65ac7cb58a3
AvgBlue <4646dudu@gmail.com>
2023-04-09 16:38:04 +0300

    commit

--- myshell.c
+++ myshell.c
@@ -317,3 +317,7 @@ int main(int argc, char *argv[])
 	// Exit the program with a return value of 0 (indicating successful execution)
 	return 0;
 }
+
+
+// /home/david/git_Repository_Drop_Place/Operating-System-ex1-gccfind/gccfind.sh /home/david/Desktop/testfolder word
+// ./gccfind.sh /home/david/Desktop/testfolder word
